8914046f4ff126ea34f9a0184270f3b7abcdc192
kholif18 <rahmadnur.kholifatur@gmail.com>
2026-03-31 14:26:03 +0700

    template

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,20 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head><base href="">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Admin Dashboard')</title>
 
+    <meta name="description" content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 94,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue &amp; Laravel versions. Grab your copy now and get life-time updates for free." />
+    <meta name="keywords" content="Metronic, bootstrap, bootstrap 5, Angular, VueJs, React, Laravel, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="Metronic - Bootstrap 5 HTML, VueJS, React, Angular &amp; Laravel Admin Dashboard Theme" />
+    <meta property="og:url" content="https://keenthemes.com/metronic" />
+    <meta property="og:site_name" content="Keenthemes | Metronic" />
+    <link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
+    <link rel="shortcut icon" href="{{  asset('admin/assets/media/logos/favicon.ico') }}" />
+    <!--begin::Fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+    <!--end::Fonts-->
+    <!--begin::Page Vendor Stylesheets(used by this page)-->
+    <link href="{{ asset('admin/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <!--end::Page Vendor Stylesheets-->
+    <!--begin::Global Stylesheets Bundle(used by all pages)-->
+    <link href="{{ asset('admin/assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('admin/assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
     <!-- Metronic CSS -->
-    <link href="{{ asset('admin/assets/css/style.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('admin/assets/css/ravaa.css') }}" rel="stylesheet" type="text/css">
     @stack('styles')
 </head>
-<body>
-    <!-- Sidebar / Header bisa di sini -->
-
-    <div class="container">
-        @yield('content')
+<body id="kt_body" class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed aside-enabled aside-fixed" style="--kt-toolbar-height:55px;--kt-toolbar-height-tablet-and-mobile:55px">
+    <!--begin::Main-->
+    <!--begin::Root-->
+    <div class="d-flex flex-column flex-root">
+			<!--begin::Page-->
+			<div class="page d-flex flex-row flex-column-fluid">
+                {{-- Sidebar --}}
+                @include('admin.layouts.sidebar')
+                {{-- end::Aside --}}
+                <!--begin::Wrapper-->
+				<div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+                    {{-- Header --}}
+                    @include('admin.layouts.header')
+                    {{-- end::Header --}}
+                    {{-- Content --}}
+                    <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+                        @yield('content')
+                    </div>
+                    {{-- end::Content --}}
+                    {{-- Footer --}}
+                    @include('admin.layouts.footer')
+                    {{-- end::Footer --}}
+                </div>
+				<!--end::Wrapper-->
+            </div>
+        <!--end::Page-->
     </div>
+    <!--end::Root-->
 
     <!-- Metronic JS -->
     <script src="{{ asset('admin/assets/js/scripts.bundle.js') }}"></script>
